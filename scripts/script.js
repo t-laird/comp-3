05ec265e32c92ae6077d07af67b7751804e4daf6
Thomas Laird <t6r6l5@gmail.com>
2017-09-18 11:54:55 -0600

    collapsing sidebar and graphs

--- scripts/script.js
+++ scripts/script.js
@@ -19,3 +19,31 @@ function sectionSelect() {
 
 $('.checkbox').on('click', checkboxToggle);
 $('li.leftnav').on('click', sectionSelect);
+
+function hideNav() {
+  if ($('header:visible').length === 1) {
+    $('header').hide();
+    $('.menu-caret1').toggleClass('fa-caret-left')
+    $('.menu-caret2').toggleClass('fa-caret-right');
+  } else {
+    $('header').show();
+    $('.menu-caret1').toggleClass('fa-caret-left');
+    $('.menu-caret2').toggleClass('fa-caret-right');
+    // $('.collapse-menu').attr('background-color','white');
+
+  }
+}
+
+$('.collapse-menu').on('click',hideNav);
+
+function hideGraphs() {
+  $('.collapse-icon').toggleClass('fa-angle-up').toggleClass('fa-angle-down');
+  if ($('.collapse-icon').hasClass('fa-angle-down')) {
+    $('.graphs').hide();
+  } else {
+    $('.graphs').show();
+  }
+}
+
+
+$('.graph-collapse-section button').on('click', hideGraphs);
